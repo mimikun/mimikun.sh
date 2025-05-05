da7a3670d805d7ae4019b348e5cf23a3a5372a85
Yuto Tanaka <karaage_nennen@icloud.com>
2025-05-05 10:08:39 +0900

    chore(powershell): update rsm

--- powershell/Invoke-RebootSecondMonitor.ps1
+++ powershell/Invoke-RebootSecondMonitor.ps1
@@ -13,6 +13,15 @@ $targetMonitorName = "BenQ EX3210U"
 
 function Invoke-RebootSecondMonitor()
 {
+    # gsudo check
+    if (-not (Get-Command gsudo -ErrorAction SilentlyContinue))
+    {
+        Write-Output "Cannot found gsudo."
+        Write-Output "Please install gsudo."
+        Write-Output "Install it: 'winget install gerardog.gsudo' or 'scoop install gsudo'"
+        return
+    }
+
     # Get monitors
     $monitors = Get-PnpDevice -Class Monitor
     $targetMonitor = $monitors | Where-Object { $_.FriendlyName -like "*$targetMonitorName*" }
@@ -21,18 +30,29 @@ function Invoke-RebootSecondMonitor()
         $id = $targetMonitor.InstanceId
         $name = $targetMonitor.FriendlyName
 
-        # Disable monitor
-        Write-Output "Disabling monitor '$($name)' ..."
-        Disable-PnpDevice -InstanceId $id -Confirm:$false
-        # Wait
-        Write-Output "Waiting...(3 sec)"
-        Start-Sleep -Seconds 3
+        # InstanceId check
+        if ($id)
+        {
+            Write-Output "InstanceId: '$($id)'"
 
-        # Enable monitor
-        Write-Output "Enabling monitor '$($name)' ..."
-        Enable-PnpDevice -InstanceId $id -Confirm:$false
+            # Disable monitor
+            Write-Output "Disabling monitor '$($name)' ..."
+            gsudo { param($deviceId) Disable-PnpDevice -InstanceId $deviceId -Confirm:$false } -args $id
 
-        Write-Output "Done! Please wait while your monitors reconnect..."
+            # Sleep
+            Write-Output "Waiting...(5 sec)"
+            Start-Sleep -Seconds 5
+
+            # Enable monitor
+            Write-Output "Enabling monitor '$($name)' ..."
+            gsudo { param($deviceId) Enable-PnpDevice -InstanceId $deviceId -Confirm:$false } -args $id
+
+            Write-Output "Done! Please wait while your monitors reconnect..."
+        } else
+        {
+            Write-Output "InstanceId is null or empty"
+            Write-Output "InstanceId: '$($id)'"
+        }
     } else
     {
         Write-Output "The specified monitor '$targetMonitorName' was not found."
