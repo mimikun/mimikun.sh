bee44ff96d047e3801e2b7b4fdc8886abb1e2d77
Yuto Tanaka <karaage_nennen@icloud.com>
2025-05-15 20:27:56 +0900

    fix(instaghext): deps-task bug

--- src/install/gh-extensions.sh
+++ src/install/gh-extensions.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-task_id=$(pueue add -p -- "echo TEMP_TASK")
+#task_id=$(pueue add -p -- "echo TEMP_TASK")
 
 while read -r line; do
     echo "Install: $line"
@@ -8,6 +8,7 @@ while read -r line; do
     if [ "$1" == "--no-pueue" ]; then
         gh extension install "$line"
     else
-        task_id=$(pueue add --after "$task_id" -p -- "gh extension install $line")
+        pueue add -- "gh extension install $line"
+        #task_id=$(pueue add --after "$task_id" -p -- "gh extension install $line")
     fi
 done <"$HOME/.mimikun-pkglists/gh_extension_list.txt"
