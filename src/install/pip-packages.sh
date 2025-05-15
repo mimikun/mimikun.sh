fa09d8ed5a5374adca205229a677df3089900b3d
Yuto Tanaka <karaage_nennen@icloud.com>
2025-05-15 20:27:56 +0900

    fix(instapip): deps-task bug

--- src/install/pip-packages.sh
+++ src/install/pip-packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-task_id=$(pueue add -p -- "echo TEMP_TASK")
+#task_id=$(pueue add -p -- "echo TEMP_TASK")
 
 while read -r line; do
     case "$line" in
@@ -10,7 +10,8 @@ while read -r line; do
         if [ "$1" == "--no-pueue" ]; then
             pip install 'thefuck @ git+https://github.com/nvbn/thefuck@62e0767c5069aeee176b0fe3459068b7703aaa26'
         else
-            task_id=$(pueue add --after "$task_id" -p -- "pip install 'thefuck @ git+https://github.com/nvbn/thefuck@62e0767c5069aeee176b0fe3459068b7703aaa26'")
+            pueue add -- "pip install 'thefuck @ git+https://github.com/nvbn/thefuck@62e0767c5069aeee176b0fe3459068b7703aaa26'"
+            #task_id=$(pueue add --after "$task_id" -p -- "pip install 'thefuck @ git+https://github.com/nvbn/thefuck@62e0767c5069aeee176b0fe3459068b7703aaa26'")
         fi
         ;;
     *)
@@ -19,7 +20,8 @@ while read -r line; do
         if [ "$1" == "--no-pueue" ]; then
             pip install "$line"
         else
-            task_id=$(pueue add --after "$task_id" -p -- "pip install $line")
+            pueue add -- "pip install $line"
+            #task_id=$(pueue add --after "$task_id" -p -- "pip install $line")
         fi
         ;;
     esac
