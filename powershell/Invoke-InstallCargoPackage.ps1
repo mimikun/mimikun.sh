37367c58dfcd4a0dd1aff5ec2ba23c5f2c3c088f
Yuto Tanaka <karaage_nennen@icloud.com>
2025-05-15 20:27:46 +0900

    feat(powershell): add deps-task

--- powershell/Invoke-InstallCargoPackage.ps1
+++ powershell/Invoke-InstallCargoPackage.ps1
@@ -1,32 +1,61 @@
-﻿function Invoke-InstallCargoPackage
+﻿# HACK: tabiew can't build
+function Invoke-UpdateTabiew()
+{
+    Write-Output "compiling ""tabiew"" takes a SO LONG time"
+    Write-Output "can't install it from crates.io"
+}
+
+# HACK: rustowl can't build
+function Invoke-UpdateRustowl()
+{
+    Write-Output "compiling ""rustowl"" takes a SO LONG time"
+    Write-Output "can't install it from crates.io"
+}
+
+function Invoke-ExistsCmd()
+{
+    Param($cmdName)
+    Get-Command -Name $cmdName > $null 2>&1
+    $result = $?
+    return $result
+}
+
+function Invoke-InstallCargoPackage
 {
     Param([switch]$NoPueue)
-
-    function Invoke-ExistsCmd()
-    {
-        Param($cmdName)
-        Get-Command -Name $cmdName > $null 2>&1
-        $result = $?
-        return $result
-    }
 
     $tmp = Join-Path -Path $env:USERPROFILE -ChildPath ".mimikun-pkglists"
     $pkglist = Join-Path -Path $tmp -ChildPath "windows_cargo_packages.txt"
 
+    #$taskId = pueue add -p -- "Write-Output start"
     Get-Content -Path $pkglist |
         ForEach-Object {
             $pkg = $_
             $cond = Invoke-ExistsCmd $pkg
             if (-not $cond)
             {
-                Write-Output "$pkg is not found"
-                if ($NoPueue)
+                switch ($pkg)
                 {
-                    cargo install $pkg
-                } else
-                {
-                    pueue add -- "cargo install $pkg"
-
+                    tabiew
+                    {
+                        Invoke-UpdateTabiew
+                    }
+                    rustowl
+                    {
+                        Invoke-UpdateRustowl
+                    }
+                    default
+                    {
+                        Write-Output "$pkg is not found"
+                        if ($NoPueue)
+                        {
+                            cargo install $pkg
+                        } else
+                        {
+                            pueue add -- "cargo install $pkg"
+                            #$taskId = pueue add --after $taskId -p -- "cargo install $pkg"
+                        }
+                    }
                 }
             }
         }
--- powershell/Invoke-UpdateCargoPackage.ps1
+++ powershell/Invoke-UpdateCargoPackage.ps1
@@ -1,4 +1,5 @@
-﻿function Get-NeedUpdateCargoPackage()
+﻿# Get a list of packages that can be updated
+function Get-NeedUpdateCargoPackage()
 {
     $tmp = cargo install-update --list |
         Select-Object -Skip 2
@@ -21,18 +22,63 @@
     return $need_update_pkgs
 }
 
+# HACK: tabiew can't build
+function Invoke-UpdateTabiew()
+{
+    Write-Output "compiling ""tabiew"" takes a SO LONG time"
+    Write-Output "can't install it from crates.io"
+}
+
+# HACK: rustowl can't build
+function Invoke-UpdateRustowl()
+{
+    Write-Output "compiling ""rustowl"" takes a SO LONG time"
+    Write-Output "can't install it from crates.io"
+}
+
+# Run update commands
 function Invoke-UpdateCargoPackage()
 {
     Param([switch]$NoPueue)
+    #$taskId = pueue add -p -- "Write-Output start"
     Get-NeedUpdateCargoPackage | ForEach-Object {
-        $pkgs = $_.Package
-        Write-Output "Update: $pkgs"
+        $pkg = $_.Package
+        Write-Output "Update: $pkg"
         if ($NoPueue)
         {
-            cargo install $pkgs
+            switch ($pkg)
+            {
+                tabiew
+                {
+                    Invoke-UpdateTabiew
+                }
+                rustowl
+                {
+                    Invoke-UpdateRustowl
+                }
+                default
+                {
+                    cargo install $pkg
+                }
+            }
         } else
         {
-            pueue add -- "cargo install $pkgs"
+            switch ($pkg)
+            {
+                tabiew
+                {
+                    Invoke-UpdateTabiew
+                }
+                rustowl
+                {
+                    Invoke-UpdateRustowl
+                }
+                default
+                {
+                    pueue add -- "cargo install $pkg"
+                    #$taskId = pueue add --after $taskId -p -- "cargo install $pkg"
+                }
+            }
         }
     }
 }
