b5d464972325b4d46dd8bed5bf60620e15d96d50
Yuto Tanaka <karaage_nennen@icloud.com>
2025-05-15 20:27:56 +0900

    fix(instapipx): deps-task bug

--- src/install/pipx-packages.sh
+++ src/install/pipx-packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-task_id=$(pueue add -p -- "echo TEMP_TASK")
+#task_id=$(pueue add -p -- "echo TEMP_TASK")
 
 while read -r line; do
     echo "Install: $line"
@@ -8,6 +8,7 @@ while read -r line; do
     if [ "$1" == "--no-pueue" ]; then
         pipx install "$line"
     else
-        task_id=$(pueue add --after "$task_id" -p -- "pipx install $line")
+        pueue add -- "pipx install $line"
+        #task_id=$(pueue add --after "$task_id" -p -- "pipx install $line")
     fi
 done <"$HOME/.mimikun-pkglists/linux_pipx_packages.txt"
