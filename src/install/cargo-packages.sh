d214b2b0aa5376944e8580fdcf64e84b7a394488
Yuto Tanaka <karaage_nennen@icloud.com>
2025-05-15 20:27:56 +0900

    fix(instacapa): deps-task bug

--- src/install/cargo-packages.sh
+++ src/install/cargo-packages.sh
@@ -4,7 +4,7 @@ function existsCmd() {
     type -a "$1" >/dev/null 2>&1
 }
 
-task_id=$(pueue add -p -- "echo TEMP_TASK")
+#task_id=$(pueue add -p -- "echo TEMP_TASK")
 
 while read -r line; do
     if ! existsCmd "$line"; then
@@ -13,7 +13,8 @@ while read -r line; do
         if [ "$1" == "--no-pueue" ]; then
             cargo install "$line"
         else
-            task_id=$(pueue add --after "$task_id" -p -- "cargo install $line")
+            pueue add -- "cargo install $line"
+            #task_id=$(pueue add --after "$task_id" -p -- "cargo install $line")
         fi
     fi
 done <"$HOME/.mimikun-pkglists/linux_cargo_packages.txt"
