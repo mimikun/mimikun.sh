f4c297702292b65771f75cd08c49102092c3b836
Yuto Tanaka <karaage_nennen@icloud.com>
2025-05-15 20:27:46 +0900

    fix(instacapa): install failed bug

--- src/install/cargo-packages.sh
+++ src/install/cargo-packages.sh
@@ -4,17 +4,38 @@ function existsCmd() {
     type -a "$1" >/dev/null 2>&1
 }
 
+# HACK: tabiew can't build
+install_cargo_tabiew() {
+    echo "compiling \"tabiew\" takes a SO LONG time"
+    echo "can't install it from crates.io"
+}
+
+# HACK: rustowl can't build
+install_cargo_rustowl() {
+    echo "compiling \"rustowl\" takes a SO LONG time"
+    echo "can't install it from crates.io"
+}
+
 #task_id=$(pueue add -p -- "echo TEMP_TASK")
 
 while read -r line; do
     if ! existsCmd "$line"; then
         echo "$line is not found"
-
-        if [ "$1" == "--no-pueue" ]; then
-            cargo install "$line"
-        else
-            pueue add -- "cargo install $line"
-            #task_id=$(pueue add --after "$task_id" -p -- "cargo install $line")
-        fi
+        case "$line" in
+        "tabiew")
+            update_cargo_tabiew
+            ;;
+        "rustowl")
+            update_cargo_rustowl
+            ;;
+        *)
+            if [ "$1" == "--no-pueue" ]; then
+                cargo install "$line"
+            else
+                pueue add -- "cargo install $line"
+                #task_id=$(pueue add --after "$task_id" -p -- "cargo install $line")
+            fi
+            ;;
+        esac
     fi
 done <"$HOME/.mimikun-pkglists/linux_cargo_packages.txt"
